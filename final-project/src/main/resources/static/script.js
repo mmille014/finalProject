5ea6b678ee6e0245297138dfb17220efe3c31e38
mmille014 <miguel_miller@cable.comcast.com>
2020-06-09 15:29:27 -0400

    Home controller and index.html added and updated.

---
+++ final-project/src/main/resources/static/script.js
@@ -0,0 +1,6 @@
+window.addEventListener ("load", function() {
+    let button = document.getElementById("enterButton");
+    button.addEventListener("click", function() {
+        console.log("Button clicked");
+    });
+});
