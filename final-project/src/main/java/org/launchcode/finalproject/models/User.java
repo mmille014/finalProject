358abfb5a59011e9434afabaf514912b0225b1ff
mmille014 <miguel_miller@cable.comcast.com>
2020-06-10 07:58:03 -0400

    Added css, a button, and created a users class

---
+++ final-project/src/main/java/org/launchcode/finalproject/models/User.java
@@ -0,0 +1,25 @@
+//package org.launchcode.finalproject.models;
+//
+//import javax.validation.constraints.NotNull;
+//
+//
+//public class User {
+//
+//    @NotNull
+//    private String username;
+//
+//    @NotNull
+//    private String pwHash;
+//
+//    public User() {}
+//
+//    public User(String username, String password) {
+//        this.username = username;
+//        this.pwHash = password;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//}
--- final-project/src/main/resources/static/script.js
+++ final-project/src/main/resources/static/script.js
@@ -1,6 +1,9 @@
 window.addEventListener ("load", function() {
-    let button = document.getElementById("enterButton");
+    let button = document.getElementById("NextButton");
     button.addEventListener("click", function() {
         console.log("Button clicked");
     });
+
+    <h1 style="color:light-green;"></h1>
+
 });
